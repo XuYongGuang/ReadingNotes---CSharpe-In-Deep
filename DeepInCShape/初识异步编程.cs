83dfb2cce01fef4d9e4530a49151fa1914e99c34
XuYongGuang <806883646@qq.com>
2018-01-07 22:40:04 +0800

    Signed-off-by: XuYongGuang <806883646@qq.com> x

---
+++ DeepInCShape/初识异步编程.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepInCShape
+{
+    class 初识异步编程
+    {
+        private static Action onInput;
+
+        static void Main(string[] args)
+        {
+            onInput += PrintPageLength;
+            Console.ReadKey();
+            onInput.Invoke();
+
+            Console.ReadKey();
+
+        }
+
+        async static void PrintPageLength()
+        {
+            int result = (await GetPageLengthAsync("http://www.bilibili.com/"));
+            Console.WriteLine(result);
+        }
+
+        static async Task<int> GetPageLengthAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Task<string> fetchTextTask = client.GetStringAsync(url);
+                int length = (await fetchTextTask).Length;
+                return length;
+            }
+        }
+    }
+}
---
+++ DeepInCShape/动态类型快速指南.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepInCShape
+{
+    class 动态类型快速指南
+    {
+        //static void Main(string[] args)
+        //{
+        //    dynamic items = new List<string>() {"first", "Second", "Third"};
+        //    dynamic stringValue = "xxx";
+        //    dynamic intValue = 2;
+        //    foreach (var i in items)
+        //    {
+        //        string stringAddString = i + stringValue;
+        //        Console.WriteLine("stringAddString:" + stringAddString);
+
+        //        string stringAddInt = i + intValue;
+        //        Console.WriteLine("stringAddInt:" + stringAddInt);
+
+        //        int intAddInt = intValue + 100;
+        //        Console.WriteLine("intAddInt:" + intAddInt);
+
+        //        Console.WriteLine("*****************************************");
+        //    }
+
+
+        //    Console.ReadKey();
+        //}
+    }
+}
